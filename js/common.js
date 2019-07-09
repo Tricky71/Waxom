a971b669ddc231bff60a6a3f72897fcf8fc0da70
Anton <nowwhat@mail.ru>
2019-07-09 11:25:19 +0400

    SVG sprite

--- js/common.js
+++ js/common.js
@@ -22,8 +22,19 @@ $(document).ready(function() {
 //     loop: true,
 //     nav: true
 //   });
- 
 
+
+// WOW - анимация при скролле
+wow = new WOW(
+		{
+		boxClass:     'wow',      // default
+		animateClass: 'animated', // default
+		offset:       0,          // default
+		mobile:       false,       // default
+		live:         true        // default
+	}
+	)
+	wow.init();
  
     
 
